96fc6c208d63e67e813d1a10401a8960db0a9977
Yan Carvalho <yancarvalhodev@gmail.com>
2026-04-29 18:06:35 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
+  site: 'https://www.adquem.com.br',
   output: "server",
   adapter: cloudflare(),
   integrations: [react()],
