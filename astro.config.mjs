d5bcc118efea5819fdfe97e056f3e13c2c84b29f
Yan Carvalho <yancarvalhodev@gmail.com>
2026-05-04 17:58:59 -0300

    Update astro.config.mjs sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import cloudflare from "@astrojs/cloudflare";
+import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
   site: 'https://www.adquem.com.br',
@@ -9,7 +10,10 @@ export default defineConfig({
   adapter: cloudflare({
     imageService: 'compile'
   }),
-  integrations: [react()],
+  integrations: [
+    react(), 
+    sitemap()
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
