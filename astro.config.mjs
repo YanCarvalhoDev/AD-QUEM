fe6678f76493e7c32d53d0d3c11c21387b779b39
Yan Carvalho <yancarvalhodev@gmail.com>
2026-09-03 22:11:44 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,15 @@ import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
-import cloudflare from "@astrojs/cloudflare";
+// import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   output: "static",
   site: "https://www.adquem.com.br", 
-  adapter: cloudflare({
-    imageService: 'compile'
-  }),
+  // adapter: cloudflare({
+  //   imageService: 'compile'
+  // }),
   integrations: [react(), markdoc(), keystatic(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
